cccda4b921bf1d23ea0ded767d353961b1367b04
DeAnna Boiani <deannaboiani@gmail.com>
2021-10-26 16:29:56 -0700

    models are updated with correct objects

--- models/index.js
+++ models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+// ProductTag = sequelize.define('product_tag', {
+//   role: Sequelize.STRING
+// });
+// Product.belongsToMany(Tag, 
+//   {through: ProductTag});
+// Product.addTag(product, { through: { id: 'product_id'}})
 
 // Tags belongToMany Products (through ProductTag)
+// Tag.belongstoMany(Product, 
+//   {through: ProductTag });
+
 
 module.exports = {
   Product,
